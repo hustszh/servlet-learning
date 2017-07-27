b8b2b90b5dd8242c795f49e26e665d741ece5ab5
suzhaohe <suzhaohe@cm-inv.com>
2017-07-27 19:20:16 +0800

    add web.xml

---
+++ servlet-started/src/main/java/me/ranjit/servlet/started/GettingStarted.java
@@ -0,0 +1,27 @@
+package me.ranjit.servlet.started;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by suzh on 7/27/2017.
+ */
+public class GettingStarted extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String name = req.getParameter("name");
+        PrintWriter pw = resp.getWriter();
+        pw.write("hello "+name+", welcome to Flash Builder's world");
+        pw.close();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
+    }
+}
